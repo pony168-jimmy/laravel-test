c56ee72288294f24280efcbe778962f256510c5f
qo7835110 <qo7835110@gmail.com>
2019-06-05 23:35:10 +0800

    4.2

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,9 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
+| 这里是您可以为您的应用程序注册web路由的地方。方法加载这些路由
+| 包含“web”中间件组的组中的 RouteServiceProvider。现在创造一些伟大的东西!
+|
 */
 
 Route::get('/', 'StaticPagesController@home');
